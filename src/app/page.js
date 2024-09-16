51136ff3a9194615cd69d906f8077b9ff9d46597
jsaraj <112096240+jsaraj@users.noreply.github.com>
2024-09-16 18:46:09 +0330

    Add Most important SEO tag to the main page

--- src/app/page.js
+++ src/app/page.js
@@ -13,25 +13,35 @@ const page = async () => {
   const movies = await getData();
 
   return (
-    <div className="flex items-center flex-wrap justify-around px-20">
-      {
-        movies.data.map((item, i) => (
-          <Link
-          href={`/movies/${item.id}`}
-          key={i} className="cursor-pointer relative w-72 h-[530px] border rounded-md my-6 overflow-hidden shadow-sm transition-all duration-200 hover:scale-105">
-            <Image alt={item.title} width={288} height={260} src={item.poster} />
-            <div className="py-1 px-2 ">
-              <h2 className="text-center pb-1 text-lg">{item.title}</h2>
-              <div className="">
-                <span className="flex gap-2 absolute bottom-2 left-2 border-orange-300 border-t w-1/2 "> <SiImdb size={20} className="mt-1" color="orange" /> {item.imdb_rating}</span>
-                <span className="absolute bottom-2 right-2 border-t border-orange-300 w-1/2 text-right">{item.year}</span>
-              </div>
-            </div>
+    <>
 
-          </Link>
-        ))
-      }
-    </div>
+      <title>Movies Website</title>
+      <meta name="description" content="This webSite is About Movies"/>
+      <meta name="robots" content="index, follow"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta charset="UTF-8"/>
+
+
+      <div className="flex items-center flex-wrap justify-around px-20">
+        {
+          movies.data.map((item, i) => (
+            <Link
+              href={`/movies/${item.id}`}
+              key={i} className="cursor-pointer relative w-72 h-[530px] border rounded-md my-6 overflow-hidden shadow-sm transition-all duration-200 hover:scale-105">
+              <Image alt={item.title} width={288} height={260} src={item.poster} />
+              <div className="py-1 px-2 ">
+                <h2 className="text-center pb-1 text-lg">{item.title}</h2>
+                <div className="">
+                  <span className="flex gap-2 absolute bottom-2 left-2 border-orange-300 border-t w-1/2 "> <SiImdb size={20} className="mt-1" color="orange" /> {item.imdb_rating}</span>
+                  <span className="absolute bottom-2 right-2 border-t border-orange-300 w-1/2 text-right">{item.year}</span>
+                </div>
+              </div>
+
+            </Link>
+          ))
+        }
+      </div>
+    </>
   );
 }
 
